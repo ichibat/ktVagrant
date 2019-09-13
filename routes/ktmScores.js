fcfc4051d23e94c75276c34a2033933fe275d16e
tim ichiba <ichibat@gmail.com>
2019-09-13 16:56:18 +0900

    edit further not tested

--- routes/ktmScores.js
+++ routes/ktmScores.js
@@ -63,7 +63,7 @@ router.get("/:ktmScoreId", authenticationEnsurer, (req, res, next) => {
 router.get("/:ktmScoreId/edit", authenticationEnsurer, (req, res, next) => {
   console.log("simple message!");
   console.log(req.params.ktmScoreId);
-  console.log("this is edit!");
+
   KtmScore.findOne({
     where: {
       ktmScoreId: req.params.ktmScoreId
@@ -71,8 +71,8 @@ router.get("/:ktmScoreId/edit", authenticationEnsurer, (req, res, next) => {
   }).then(ktmScore => {
     if (isMine(req, ktmScore)) {
       // 作成者のみが編集フォームを開ける
-      console.log("before patientID");
-      console.log(ktmScore.patientID);
+      console.log("next is ktmScore");
+      console.log(ktmScore);
 
       res.render("edit", {
         user: req.user,
@@ -89,5 +89,52 @@ router.get("/:ktmScoreId/edit", authenticationEnsurer, (req, res, next) => {
 function isMine(req, ktmScore) {
   return ktmScore && parseInt(ktmScore.createdBy) === parseInt(req.user.id);
 }
+
+router.post("/:ktmScoreId", authenticationEnsurer, (req, res, next) => {
+  KtmScore.findOne({
+    where: {
+      ktmScoreId: req.params.ktmScoreId
+    }
+  }).then(ktmScore => {
+    if (ktmScore && isMine(req, ktmScoreId)) {
+      if (parseInt(req.query.edit) === 1) {
+        const updatedAt = new Date();
+        ktmScore
+          .update({
+            ktmScoreId: ktmScoreId,
+            patientID: req.body.patientID.slice(0, 255) || "（未入力）",
+            Q1Value: req.body.question1,
+            Q2Value: req.body.question2,
+            Q3Value: req.body.question3,
+            Q4Value: req.body.question4,
+            Q5Value: req.body.question5,
+            Q6Value: req.body.question6,
+            Q7Value: req.body.question7,
+            Q8Value: req.body.question8,
+            Q9Value: req.body.question9,
+            Q10Value: req.body.question10,
+            Q11Value: req.body.question11,
+            Q12Value: req.body.question12,
+            Q13Value: req.body.question13,
+            createdBy: req.user.id,
+            updatedAt: updatedAt
+          })
+          .then(ktmScore => {
+            res.redirect("/ktmScores/" + ktmScore.ktmScoreId);
+          });
+      } else {
+        const err = new Error("不正なリクエストです");
+        err.status = 400;
+        next(err);
+      }
+    } else {
+      const err = new Error(
+        "指定された予定がない、または、編集する権限がありません"
+      );
+      err.status = 404;
+      next(err);
+    }
+  });
+});
 
 module.exports = router;
